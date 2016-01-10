796b56db5ad2b0e11c7e852011f2a5a493ca3aba
Arjay Sitoy <sitoy.arjay@gmail.com>
2016-01-11 03:41:53 +0800

    Prototype Phase
    * Use expressjs for http connection between processes
    * Use commanderjs for the command-line interface

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 var config = require('./conf/config.json'),
     validator = require('./lib/config-validator'),
-    child_process = require('child_process');
+    child_process = require('child_process'),
+    cluster = [],
+    express = require('express')();
 
-/*
- * TODO
- * - Make checks global for easy instance monitoring
- * - Make alerts global for easy access
- * - Add Alert Object for email capabilities
- * - Add on exit event for clean exit when exiting this process
- * - Fix config data checker
- *
- *
- * A Testing Framework (Mocha, Vows, Intern)
- * An Assertion Library (Chai, Assert)
- * Stubs (Sinon)
- * Module Control (Mockery, Rewire)
- *
- * http://fredkschott.com/post/2014/05/nodejs-testing-essentials/
- *
- * /
+express.get('/start', function (res, req) {
+    validator.validate(config);
 
+    config.checks.forEach(function (check) {
+        var worker = child_process.fork(__dirname + '/lib/checker.js');
+        check.alerts = config.alerts;
+        worker.send(check);
 
- /* validate config */
-validator.validate(config);
-
-config.checks.forEach(function (check) {
-    var worker = child_process.fork(__dirname + '/lib/checker.js');
-    check.alerts = config.alerts;
-    worker.send(check);
+        cluster.push(worker);
+    });
+    res.send('OK');
 });
+
+express.get('/stop', function (res, req) {
+    /* stop application */
+});
+
+express.get('/status', function (res, req) {
+    var list = {};
+    cluster.forEach(function (worker) {
+        worker.send('status');
+        worker.on('message', function (instance) {
+            list[instance.getConfig().id] = instance.getStatus().content;
+        });
+    });
+
+    res.send(JSON.stringify(list));
+});
+
+express.listen(4321, '127.0.0.1', function () {
+    console.log("... port %d in %s mode", 4321, express.settings.env);
+});
+
