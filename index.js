e734077019fce3155402c94f6fc6868587cfd41e
Arjay Sitoy <sitoy.arjay@gmail.com>
2016-01-12 02:22:22 +0800

    Socket.IO for worker - master data passing

--- index.js
+++ index.js
@@ -2,38 +2,32 @@ var config = require('./conf/config.json'),
     validator = require('./lib/config-validator'),
     child_process = require('child_process'),
     cluster = [],
-    express = require('express')();
+    sockets = [],
+    express = require('express')(),
+    async = require('async'),
+    server = require('socket.io')(4321);
 
-express.get('/start', function (res, req) {
-    validator.validate(config);
+validator.validate(config);
 
-    config.checks.forEach(function (check) {
-        var worker = child_process.fork(__dirname + '/lib/checker.js');
-        check.alerts = config.alerts;
-        worker.send(check);
+server.on('connection', function (socket) {
+    socket.emit('config', cluster);
+    sockets.push(socket);
+});
 
-        cluster.push(worker);
+config.checks.forEach(function (check) {
+    cluster.push({
+        config: check,
+        worker: child_process.fork(__dirname + '/lib/worker.js')
     });
-    res.send('OK');
 });
 
-express.get('/stop', function (res, req) {
-    /* stop application */
-});
-
-express.get('/status', function (res, req) {
-    var list = {};
-    cluster.forEach(function (worker) {
-        worker.send('status');
-        worker.on('message', function (instance) {
-            list[instance.getConfig().id] = instance.getStatus().content;
-        });
+setTimeout(function () {
+    sockets.forEach(function (socket, index) {
+        socket.emit('config', cluster[index].config);
     });
+}, 3000);
 
-    res.send(JSON.stringify(list));
-});
 
-express.listen(4321, '127.0.0.1', function () {
-    console.log("... port %d in %s mode", 4321, express.settings.env);
-});
+
+
 
