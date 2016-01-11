78971cba900369a156daf6c217811eebf655168b
Arjay Sitoy <sitoy.arjay@gmail.com>
2016-01-12 04:09:58 +0800

    Beta Release
    * Add npm start, stop scripts
    * Remove unnecessary commands from status-check
    * Remove unnecessary project dependencies(package.json)

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ var config = require('./conf/config.json'),
     child_process = require('child_process'),
     cluster = [],
     sockets = [],
-    express = require('express')(),
     async = require('async'),
-    server = require('socket.io')(4321);
+    server = require('socket.io')(4321),
+    status = require('socket.io')(4322),
+    report = [];
 
 validator.validate(config);
 
 server.on('connection', function (socket) {
+    socket.on('status', function (status) {
+        report.push(status);
+    });
     sockets.push(socket);
 });
 
@@ -18,4 +22,20 @@ config.checks.forEach(function (check) {
     check.alerts = config.alerts;
     worker.send(check);
     cluster.push(worker);
+});
+
+status.on('connection', function (socket) {
+    socket.on('status', function () {
+        async.eachSeries(sockets, function (socket, callback) {
+            socket.emit('status');
+            callback();
+        }, function (err) {
+            if (err) {
+                throw err;
+            } else {
+                socket.emit('status', report);
+                report = [];
+            }
+        });
+    });
 });
